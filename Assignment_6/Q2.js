096141e577410ee972dec4b635b1f4d16f70bdd7
AshishFinalap <145488160+AshishFinalap@users.noreply.github.com>
2025-02-04 14:58:50 +0530

    Q2.js

---
+++ Assignment_6/Q2.js
@@ -0,0 +1,7 @@
+function findLargest(a, b, c) {
+  return Math.max(a, b, c);
+}
+a= prompt("Enter the first value ");
+b = prompt("Enter the second value ");
+c=prompt("Enter the third number");
+console.log(findLargest(a, b, c)); 
